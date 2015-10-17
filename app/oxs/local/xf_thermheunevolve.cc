109919b2d074e0c3f442647e297ab98c698873ea
xfong <xuanyao.fong@gmail.com>
2015-10-17 12:26:31 +0800

    Update xf_thermheunevolve.cc
    Update to be OOMMF 1.2a6 compatible

--- app/oxs/local/xf_thermheunevolve.cc
+++ app/oxs/local/xf_thermheunevolve.cc
@@ -75,8 +75,8 @@ Xf_ThermHeunEvolve::Xf_ThermHeunEvolve(
     max_step_decrease(0.03125), max_step_increase_limit(4.0),
     max_step_increase_adj_ratio(1.9),
     reject_goal(0.05), reject_ratio(0.05),
-    KBoltzmann(1.38062e-23),tmpStepsize(1e-17),
-    has_tempscript(0),temperature(0.),
+    KBoltzmann(1.38062e-23),temperature(0.),
+    tmpStepsize(1e-17),has_tempscript(0),
     energy_state_id(0),next_timestep(1e-27),
     rkstep_ptr(NULL)
 {
@@ -337,7 +337,7 @@ void Xf_ThermHeunEvolve::UpdateMeshArrays(const Oxs_Mesh* mesh)
   }
 
   const OC_INDEX size = mesh->Size();
-  OC_INDEX i,j;
+  OC_INDEX i;
 
   alpha_init->FillMeshValue(mesh,alpha);
   gamma_init->FillMeshValue(mesh,gamma);
@@ -1028,7 +1028,7 @@ void Xf_ThermHeunEvolve::TakeRungeKuttaStep2Heun
   GetEnergyDensity(next_state_key.GetReadReference(),temp_energy,
                    &vtmpA,NULL,pE_pt);
   UpdateHfluct(cstate->mesh);// Update the random field before calculating slopes
-  Oxs_SimState& workstate = next_state_key.GetWriteReference();
+  // Oxs_SimState& workstate = next_state_key.GetWriteReference();
   tmpStepsize = stepsize;
   Calculate_dm_dt(next_state_key.GetReadReference(),vtmpA,pE_pt,
                   vtmpA,max_dm_dt,dE_dt,timestep_lower_bound);
